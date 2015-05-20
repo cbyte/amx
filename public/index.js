58089dd600f88e2e0862211d83eb1260c100cf2a
jnncks <jannickscherf@gmx.de>
2015-05-20 11:52:17 +0200

    Add hsl to hex conversion
    hsl is not supported on all devices :(

--- public/index.js
+++ public/index.js
@@ -17,10 +17,9 @@ function handlerDeviceOrientation(e) {
             + "pitch: " + Math.round(e.beta) + "\n"
             + "yaw: "+ Math.round(e.alpha) + "\n"
   document.getElementById("logOrientation").innerHTML = debug
-  var h = e.alpha + 0.4 * e.beta - 0.4 * e.gamma;
-  if (h > 360) h = 360;
-  var color = "hsl("+e.alpha+", 100%, 65%);"
-  document.body.style.backgroundColor = color
+  var h = min((e.alpha + 0.4 * e.beta - 0.4 * e.gamma)/360), 1);
+  var color = hslToHex(h, 1, 0.65) //"hsl("+h+", 100%, 65%);"
+  document.body.style.background = color
   socket.emit('orientation', {roll: Math.round(e.gamma),
     pitch: Math.round(e.beta),
     yaw: Math.round(e.alpha)});
@@ -32,4 +31,36 @@ function handlerDeviceMotion(e) {
             + "rotation rate (X,Y,Z): " + Math.round(e.rotationRate.beta) + ', ' + Math.round(e.rotationRate.gamma) + ', ' + Math.round(e.rotationRate.alpha) + "\n"
             + "refresh interval: " + e.interval
   document.getElementById("logMotion").innerHTML = debug
+}
+
+/*
+hsl to hex functions from http://jsperf.com/hsl-to-hex
+*/
+function componentToHex(c) {
+  c = Math.round(c * 255).toString(16);
+  return c.length === 1 ? "0" + c : c;
+}
+
+function hslToHex(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hueToRGB(p, q, h + 1 / 3);
+    g = hueToRGB(p, q, h);
+    b = hueToRGB(p, q, h - 1 / 3);
+  }
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hueToRGB(p, q, t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
 }
